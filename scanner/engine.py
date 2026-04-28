45bd37e22b3c743198afca2ed4ba21ca1f4469ee
fareeza05 <fareezarahman123@gmail.com>
2026-04-28 14:35:38 -0400

    added scan engine + reporter boilerplate

---
+++ scanner/engine.py
@@ -0,0 +1,7 @@
+class ScanEngine:
+    def __init__(self, target_path):
+        self.target_path = target_path
+
+    def run_all_checks(self):
+        # REGEX LOGIC TO BE ADDED
+        return []
---
+++ scanner/reporter.py
@@ -0,0 +1,3 @@
+def generate_report(findings):
+    print(f"DEBUG: Generating report for {len(findings)} findings...")
+    # Will add JSON export logic here later
